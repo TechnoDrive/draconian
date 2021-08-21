5e3b51d8819f500843f91b6fb743770b7ad933b4
Hextanium <hextanium.on.discord@gmail.com>
2021-08-21 14:39:39 -0400

    [add] `Token` struct

---
+++ Language/Location.cs
@@ -0,0 +1,13 @@
+﻿namespace Implementation.Language {
+    public struct Location {
+        public string File;
+        public int Line;
+        public int Column;
+
+        public Location(string file, int line, int column) {
+            File = file;
+            Line = line;
+            Column = column;
+        }
+    }
+}
--- Lexer/Token/Token.cs
+++ Lexer/Token/Token.cs
@@ -1,5 +1,19 @@
-﻿namespace Implementation.Lexer.Token {
+﻿using Implementation.Language;
+
+namespace Implementation.Lexer.Token {
     public struct Token {
-        
+        public TokenType Type;
+        public string Lexeme;
+        public Location Location;
+
+        public Token(TokenType type, string lexeme, Location location) {
+            Type = type;
+            Lexeme = lexeme;
+            Location = location;
+        }
+
+        public override string ToString() {
+            return $"Token {{ Type: {Type}, Lexeme: {Lexeme}, Location: {Location} }}";
+        }
     }
 }
