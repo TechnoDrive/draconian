331bf0462934c22b1d0cb96bc203d00194e62da8
Hextanium <hextanium.on.discord@gmail.com>
2021-08-21 14:32:19 -0400

    [add] basic tokentypes, will add more later

--- Lexer/Token/TokenType.cs
+++ Lexer/Token/TokenType.cs
@@ -1,5 +1,53 @@
 ﻿namespace Implementation.Lexer.Token {
     public enum TokenType {
+        Plus,
+        Minus,
+        Mult,
+        Div,
         
+        Dot,
+        Comma,
+        Colon,
+        Semicolon,
+        
+        Not,
+        Equal,
+        LessThan,
+        GreaterThan,
+        
+        NotEqual,
+        DoubleEqual,
+        LessThanEqual,
+        GreaterThanEqual,
+
+        Comment,
+        Identifier,
+        Number,
+        String,
+        
+        Abstract,
+        Pub,
+        Class,
+        Parent,
+        This,
+        Fun,
+        Let,
+        Val,
+        If,
+        Else,
+        For,
+        While,
+        When,
+        ForEach,
+        As,
+        Import,
+        Export,
+        
+        LeftParen,
+        LeftBrace,
+        LeftBracket,
+        RightParen,
+        RightBrace,
+        RightBracket,
     }
 }
