e1b3aae932a551ff70a8099f0bbd0caf7178f677
Hextanium <hextanium.on.discord@gmail.com>
2021-08-21 16:36:08 -0400

    [change] `Program` to lex any file given as input

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using Implementation.Lexer.Token;
 
 namespace Implementation {
     internal static class Program {
         public static void Main(string[] args) {
             if (args.Length != 2) {
                 Console.WriteLine("Usage: draconian path/to/input.dracon path/to/output.js");
+            } else {
+                Lexer.Lexer lexer = new Lexer.Lexer();
+                foreach (Token token in lexer.Tokenize(System.IO.File.ReadAllText(args[0]))) {
+                    Console.WriteLine(token);
+                }
             }
         }
     }
